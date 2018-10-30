dc70060118656ebf339f444300791ee38536679b
Júlio <julio.mendes@sensedia.com>
2018-10-30 16:17:40 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/br/com/juliomendes90/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,12 @@
+package br.com.juliomendes90.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+}
